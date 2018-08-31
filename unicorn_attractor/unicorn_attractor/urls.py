69603cdaba90741f9e12f407a74968145060c6cd
Colman Leung <comacoma0218@hotmail.com>
2018-08-31 17:59:25 +0100

    Added "about.html"

--- unicorn_attractor/unicorn_attractor/urls.py
+++ unicorn_attractor/unicorn_attractor/urls.py
@@ -15,9 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from home.views import index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', index, name="index"),
+    path('', include('home.urls')),
 ]
