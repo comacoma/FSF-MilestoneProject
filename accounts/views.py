8b98453661f9cf599979085b6c42cf271691a114
Colman Leung <comacoma0218@hotmail.com>
2018-09-01 20:14:11 +0100

    Added 'accounts' app

---
+++ accounts/views.py
@@ -0,0 +1,33 @@
+from django.shortcuts import render, redirect, reverse
+from django.contrib import auth, messages
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from accounts.forms import *
+
+# Create your views here.
+@login_required
+def logout(request):
+    auth.logout(request)
+    messages.success(request, "You have successfully logged out.")
+    return redirect(reverse('index'))
+
+def login(request):
+    """Return a login page"""
+    if request.user.is_authenticated:
+        return redirect(reverse('index'))
+
+    if request.method == "POST":
+        login_form = UserLoginForm(request.POST)
+
+        if login_form.is_valid():
+            user = auth.authenticate(username=request.POST['username'],
+                                     password=request.POST['password'])
+            if user:
+                auth.login(user=user, request=request)
+                messages.success(request, "You have successfully logged in!")
+                return redirect(reverse('index'))
+            else:
+                login_form.add_error(None, "Your username or password is incorrect")
+    else:
+        login_form = UserLoginForm()
+    return render(request, "login.html", {"login_form": login_form})
