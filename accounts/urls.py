036ba43de87e7d0027559ad4b58e7ae1cc565be0
Colman Leung <comacoma0218@hotmail.com>
2018-09-02 12:02:42 +0100

    Adding password reset feature (backend not implemented yet)

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,18 @@
-from django.urls import path
+from django.urls import path, include
 from .views import *
+from django.contrib.auth.views import *
 
 urlpatterns = [
     path('login/', login, name="login"),
     path('logout/', logout, name="logout"),
     path('register/', registration, name="registration"),
     path('profile/', user_profile, name="profile"),
+    path('password/', include([
+        path('password_change/', PasswordChangeView.as_view(), name='password_change'),
+        path('password_change/done/', PasswordChangeDoneView.as_view(), name='password_change_done'),
+        path('password_reset/', PasswordResetView.as_view(), name='password_reset'),
+        path('password_reset/done/', PasswordResetDoneView.as_view(), name='password_reset_done'),
+        path('reset/<uidb64>/<token>/', PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+        path('reset/done/', PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+    ])),
 ]
