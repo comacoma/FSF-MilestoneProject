581d2c1cc42f931b455ef1bcb2987e19f1403709
Colman Leung <comacoma0218@hotmail.com>
2018-09-25 18:14:06 +0100

    refined admin page for better security

--- issuetracker/admin.py
+++ issuetracker/admin.py
@@ -1,15 +1,28 @@
 from django.contrib import admin
-from .models import Ticket, Comment, Fund
+from .models import Ticket, Comment, Fund, ProgressLog
 
 # Register your models here.
 class CommentInline(admin.TabularInline):
     model = Comment
+    readonly_fields = ('author', 'comment_date',)
 
 class TicketAdmin(admin.ModelAdmin):
     inlines = [
         CommentInline,
     ]
+    readonly_fields = (
+        'author',
+        'title',
+        'type',
+        'upvote_user',
+        'submission_date',
+        'last_modified',
+        'upvote_fund',
+    )
+
+class FundAdmin(admin.ModelAdmin):
+    readonly_fields = ('ticket', 'user', 'fund',)
 
 admin.site.register(Ticket, TicketAdmin)
-admin.site.register(Comment)
-admin.site.register(Fund)
+admin.site.register(Fund, FundAdmin)
+admin.site.register(ProgressLog)
