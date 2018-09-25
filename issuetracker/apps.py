03f1fb9b64c116da9a47c2da974346bb48c07560
Colman Leung <comacoma0218@hotmail.com>
2018-09-25 18:13:49 +0100

    update issuetracker app settings

--- issuetracker/apps.py
+++ issuetracker/apps.py
@@ -1,5 +1,6 @@
 from django.apps import AppConfig
 
-
 class IssueTrackerConfig(AppConfig):
-    name = 'issue_tracker'
+    name = 'issuetracker'
+    label = 'issuetracker'
+    verbose_name = "Issue Tracker"
