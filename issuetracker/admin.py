f06f04606879b90a27f1826c506195133fe6f63a
Colman Leung <comacoma0218@hotmail.com>
2018-10-09 12:42:37 +0100

    refine issuetracker model admin

--- issuetracker/admin.py
+++ issuetracker/admin.py
@@ -11,13 +11,27 @@ class TicketAdmin(admin.ModelAdmin):
         CommentInline,
     ]
     readonly_fields = (
-        'author',
-        'type',
+        'upvote_fund',
         'upvote_user',
         'submission_date',
         'last_modified',
-        'upvote_fund',
     )
+    exclude = (
+        'upvote_fund',
+        'upvote_user',
+        'submission_date',
+        'last_modified',
+    )
+
+    """
+    Referring code from:
+    https://stackoverflow.com/questions/17613559/django-readonly-field-only-on-change-but-not-when-creating
+    """
+    def get_readonly_fields(self, request, obj=None):
+        if obj: #This is the case when obj is already created i.e. it's an edit
+            return ['author', 'type']
+        else:
+            return []
 
 class FundAdmin(admin.ModelAdmin):
     readonly_fields = ('ticket', 'user', 'fund',)
