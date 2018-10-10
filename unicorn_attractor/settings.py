dfff1baeb4a4fea6e3eda87ab55194127e5f1fb7
Colman Leung <comacoma0218@hotmail.com>
2018-10-10 15:52:30 +0100

    fixing media url

--- unicorn_attractor/settings.py
+++ unicorn_attractor/settings.py
@@ -160,7 +160,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
 
-MEDIA_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, MEDIAFILES_LOCATION)
+MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # EMAIL_BACKEND = "django.core.mail.backends.filebased.EmailBackend"
