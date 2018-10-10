f8f31c3683e72d5b6592c9f0b612616ebf0c0166
Colman Leung <comacoma0218@hotmail.com>
2018-10-10 14:31:05 +0100

    added new allowed host

--- unicorn_attractor/settings.py
+++ unicorn_attractor/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', os.environ.get('C9_HOSTNAME')]
+ALLOWED_HOSTS = ['127.0.0.1', os.environ.get('C9_HOSTNAME'), 'fsf-milestone.herokuapp.com']
 
 
 # Application definition
